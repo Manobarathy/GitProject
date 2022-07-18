3cb50858d1346388546bd15d4698849357d880e7
Manobarathy <manobarathy89@gmail.com>
2022-07-18 19:02:01 +0530

    first commit

---
+++ src/org/array/test/login/ArrayOned.java
@@ -0,0 +1,23 @@
+package org.array.test.login;
+
+public class ArrayOned {
+public static void main(String[] args) {
+//int a[] = new int[4];
+String a[][]= {{"*","*"},{"@","@"}};
+//System.out.println(a[4]);
+int b = a.length;
+System.out.println(b);
+System.out.println("\t" +a[0][0]);
+System.out.println(a[0][0] + "\t\t" + a[0][0]);
+
+//for(int i=0;i<2;i++)
+//{
+	//for(int j=0;j<2;j++) {
+//	System.out.print(a[i][j]+"\t ");
+//}
+//System.out.println();
+//for (int x : a) {
+	//System.out.println(x);
+//}
+}
+}
